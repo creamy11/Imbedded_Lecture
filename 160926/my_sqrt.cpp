393d6bf0e73808dfa28dedd6f8b0e24de8625862
Jiyeon <suh91919@naver.com>
2016-09-27 14:17:10 +0900

    160926_mysqrt

---
+++ 160926/my_sqrt.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	double x = 2;
+	double s = 1;
+
+	for (int i = 0; i < 10; i++)
+	{
+		s = 0.5*(s + x / s);
+
+	}
+
+	printf("mysqrt(%f) = (%f)\n", x, s);
+
+	return 0;
+}
