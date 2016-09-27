4c0f679c47b583cb2e7d250204537f7abb66d2b9
Jiyeon <suh91919@naver.com>
2016-09-27 14:39:17 +0900

    Add_stopping_criteria

--- 160926/my_sqrt/my_sqrt/my_sqrt.cpp
+++ 160926/my_sqrt/my_sqrt/my_sqrt.cpp
@@ -1,20 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	double x = 2;
 	double s = 1;
 
+	int maxiter = 100;
+	double tol = 1.e-14;
 	int i = 0;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < maxiter; i++)
 	{
 		printf("Before iteration %d, s = %f\n", i, s);
+		double s0 = s;
 		s = 0.5*(s + x / s);
 
+		double delta_s = s - s0;
+		if (fabs(delta_s / x) < tol)
+			break;
 	}
 	printf("After %d iterations, s = %f\n", i, s);
 
-	printf("mysqrt(%f) = (%f)\n", x, s);
+	printf("\nmysqrt(%f) = (%f)\n", x, s);
 
 	return 0;
 }
