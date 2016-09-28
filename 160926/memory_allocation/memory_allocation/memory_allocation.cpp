44c89d3553eff19acdacb3f2f88a76bb9a776cd0
Jiyeon <suh91919@naver.com>
2016-09-28 10:38:02 +0900

    pointer 1

--- 160926/memory_allocation/memory_allocation/memory_allocation.cpp
+++ 160926/memory_allocation/memory_allocation/memory_allocation.cpp
@@ -3,16 +3,17 @@
 
 int main()
 {
-	double*pX;
-	int n = 1024;
-	pX = (double*)malloc(n * sizeof(double));
+	double*pX; //포인터 선언
+	int n = 1; //포인터 사이즈
+	pX = (double*)malloc(n * sizeof(double)); //포인터에 메모리 할당
 
-	if (pX == NULL)
-	{
-		printf("Insufficient memory\n");
-		return 0;
-	}
-		
-	free(pX);
+	double X = 11; //포인터가 가리킬 값
+	pX = &X; //포인터에 X의 주소를 넣는다.
+	
+	printf("X 의 주소: %d\n", &X);
+	printf("X 값: %f\n", X);	
+	printf("pX 값: %d\n", pX);
+	printf("pX 가 가리키는 값: %f\n", *pX);
+	
 	return 0;
 }
